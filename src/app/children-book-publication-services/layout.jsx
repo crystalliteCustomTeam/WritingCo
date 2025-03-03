135f62aebea8906f124cd91971dbe135d9d8e2c4
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2025-03-03 15:21:47 +0500

    page contetn udpated book publication

--- src/app/children-book-publication-services/layout.jsx
+++ src/app/children-book-publication-services/layout.jsx
@@ -16,11 +16,7 @@ export const metadata = {
     },
     //===== Canonical =====
     alternates: { canonical: '/children-book-publication-services' },
-    //===== No-Index =====
-    robots: {
-        index: false,
-        follow: false,
-    }
+
 }
 
 
